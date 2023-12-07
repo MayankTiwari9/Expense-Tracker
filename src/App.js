6d65cdd830eb8e81529ce7bde1d1dff3f68fbea3
MayankTiwari9 <mt1027319@gmail.com>
2023-12-08 01:13:10 +0530

    Expense Added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import LogInForm from "./Components/Form/LogInForm";
 import Welcome from "./Components/Welcome/Welcome";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import ForgotPassword from "./Components/Form/ForgotPassword";
+import Expense from "./Components/Expense/Expense";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LogInForm />} />
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/updateprofile" element={<UserProfile/>}/>
         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+        {token && <Route path="/expense" element={<Expense/>}/>}
+        
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Expense/Expense.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+const Expense = () => {
+  const [amount, setAmount] = useState();
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpenseHandler = (e) => {
+    e.preventDefault();
+
+    const newExpense = {
+      amount: amount,
+      description: description,
+      category: category,
+    };
+
+    setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+
+    setAmount("");
+    setDescription("");
+    setCategory("");
+  };
+
+  return (
+    <>
+      <form
+        className="d-flex flex-column w-50 mx-auto align-items-center justify-content-center rounded text-white"
+        style={{ backgroundColor: "rgb(3,21,37)" }}
+        onSubmit={addExpenseHandler}
+      >
+        <div className="d-flex mt-3 w-100 justify-content-center">
+          <label>Amount Spent:- </label>
+          <input
+            className="ml-4"
+            style={{ marginLeft: "10px" }}
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="d-flex mt-3 w-100 justify-content-center">
+          <label>Description:- </label>
+          <input
+            className="ml-4"
+            style={{ marginLeft: "10px" }}
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="d-flex mt-3 w-100 justify-content-center">
+          <label>Category:- </label>
+          <select
+            className="ml-4"
+            style={{ marginLeft: "10px" }}
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Food">Food</option>
+            <option value="Petrol">Petrol</option>
+            <option value="Emi">Emi</option>
+            <option value="House Rent">House Rent</option>
+          </select>
+        </div>
+        <div>
+          <button className="btn btn-secondary m-3" type="submit">
+            Add Expense
+          </button>
+        </div>
+      </form>
+      <div
+        className="d-flex flex-column w-50 mx-auto align-items-center justify-content-center mt-3 rounded text-white"
+        style={{ backgroundColor: "rgb(3,21,37)" }}
+      >
+        {expenses &&
+          expenses.map((item, index) => {
+            return (
+              <div className="d-flex justify-content-around align-items-center w-100" key={index}>
+                <h3 className="align-middle">{item.amount}</h3>
+                <p className="">{item.description}</p>
+                <p className="">{item.category}</p>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Expense;
